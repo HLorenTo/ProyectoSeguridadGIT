0ab5a45cb79dec59ddf0505138df929e4b680430
HLorenTo <lorena.torres.h@hotmail.com>
2020-10-15 20:44:18 -0500

    3 fotografia

--- src/proyectoseguridadgit/Matematicas.java
+++ src/proyectoseguridadgit/Matematicas.java
@@ -10,5 +10,22 @@ package proyectoseguridadgit;
  * @author loren
  */
 public class Matematicas {
-    
+    public int sumar (int i, int j){
+        return i +j;
+    }
+    public int restar (int i, int j){
+        return i -j;    
+    }
+    public int multiplicar (int i, int j){
+        return i *j;    
+    }
+    public int dividir (int i, int j){
+        return i /j;    
+    }
+    public int dividir2 (int i, int j){
+        return i /j;    
+    }
+    public int dividir3 (int i, int j){
+        return i /j;    
+    }
 }
